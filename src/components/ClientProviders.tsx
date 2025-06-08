ffca51e78a5f1cb7d8cb8cc1915d4bd84bac2feb
Anmol Srivastava <a4anmol93.6@gmail.com>
2025-06-08 18:19:42 +0530

    fix lints

---
+++ src/components/ClientProviders.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { trpc } from '@/utils/trpc';
+import { httpBatchLink } from '@trpc/client';
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+
+interface ClientProvidersProps {
+  children: React.ReactNode;
+}
+
+const ClientProviders: React.FC<ClientProvidersProps> = ({ children }) => {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min.js' as any);
+  }, []);
+
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: { 
+      queries: {
+        staleTime: 5 * 1000,
+      },
+    },
+  }));
+
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: '/api/trpc',
+        }),
+      ],
+    })
+  );
+
+  return (
+    <UserProvider>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </trpc.Provider>
+    </UserProvider>
+  );
+};
+
+export default ClientProviders; 
